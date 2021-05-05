fb2f9a7f5803fa55213d20c0b61f354f54d6030f
EdanDanino <idan25100@gmail.com>
2021-05-05 20:11:42 +0300

    fix to nav bars routing

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,10 @@
 import { List, ListItem, ListItemText } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
-import {
-  createStyles,
-  fade,
-  makeStyles,
-  Theme,
-} from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import React, { FunctionComponent, useMemo } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { navbarTypes } from "./types";
 
@@ -23,45 +18,6 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "none",
       [theme.breakpoints.up("sm")]: {
         display: "block",
-      },
-    },
-    search: {
-      position: "relative",
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(1),
-        width: "auto",
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "12ch",
-        "&:focus": {
-          width: "20ch",
-        },
       },
     },
     navDisplayFlex: {
@@ -86,10 +42,6 @@ const StyledLink = styled(Link)`
 
 const Navbar: FunctionComponent<navbarTypes> = ({ navLinks }) => {
   const classes = useStyles();
-  const location = useLocation();
-  const route = useMemo(() => {
-    return location?.pathname.split("/")[1];
-  }, [location]);
 
   const { title, navDisplayFlex } = classes;
 
@@ -107,7 +59,7 @@ const Navbar: FunctionComponent<navbarTypes> = ({ navLinks }) => {
           >
             {navLinks.map(({ title }) => {
               return (
-                <StyledLink key={title} to={`${route}/${title}`}>
+                <StyledLink key={title} to={`/${title}`}>
                   <ListItem button>
                     <ListItemText primary={title} />
                   </ListItem>
