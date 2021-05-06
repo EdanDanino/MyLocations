1d32f8a2b69ab3fcc7ec7a6ec56701d80bee5459
EdanDanino <idan25100@gmail.com>
2021-05-06 23:24:06 +0300

    created Edit pages

---
+++ src/pages/EditPages/EditCategory.tsx
@@ -0,0 +1,60 @@
+import { Form } from "components";
+import _ from "lodash";
+import React, { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+import { updateCategory } from "store/slices/category";
+import { categoryStateType, StateType } from "store/slices/types";
+import styled from "styled-components";
+
+const Root = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const selector = (state: StateType) => ({
+  categories: state.categories,
+});
+
+const fields = [
+  {
+    name: "name",
+  },
+];
+
+const EditCategory = () => {
+  const { categories } = useSelector(selector);
+  const location = useLocation();
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const itemId = useMemo(() => {
+    return location?.pathname.split("/")[3];
+  }, [location]);
+
+  const categoryItem =
+    _.find(categories, (o: { id: string }) => {
+      return o.id === itemId;
+    }) || categories;
+
+  const onSubmitEditCategory = (data: any) => {
+    const newCategoryWithUpdates = { ...categoryItem, ...data };
+    dispatch(updateCategory(newCategoryWithUpdates));
+    history.push("/Categories");
+  };
+
+  return (
+    <Root>
+      <Form
+        fields={fields}
+        onSubmit={onSubmitEditCategory}
+        initialValues={categoryItem as categoryStateType}
+      />
+    </Root>
+  );
+};
+
+export default EditCategory;
---
+++ src/pages/EditPages/EditLocation.tsx
@@ -0,0 +1,87 @@
+import { Form } from "components";
+import _ from "lodash";
+import React, { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+import { updateLocation } from "store/slices/location";
+import { locationKey, locationStateType, StateType } from "store/slices/types";
+import styled from "styled-components";
+
+const Root = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const selector = (state: StateType) => ({
+  locations: state.locations,
+  categories: state.categories,
+});
+
+const EditLocation = () => {
+  const { locations, categories } = useSelector(selector);
+  const location = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const fields = useMemo(
+    () => [
+      {
+        name: "name",
+      },
+      {
+        name: "address",
+      },
+      {
+        name: "lat",
+      },
+      {
+        name: "lng",
+      },
+      {
+        name: "category",
+        isFieldASelect: true,
+        options: categories,
+      },
+    ],
+    [categories]
+  );
+
+  const itemId = useMemo(() => {
+    return location?.pathname.split("/")[3];
+  }, [location]);
+
+  const locationItem = _.find(locations, (o: { id: string }) => {
+    return o.id === itemId;
+  });
+
+  const onSubmitEditLocation = (data: any) => {
+    const newLocationWithUpdates = { ...data };
+    if (locationItem) {
+      Object.keys(locationItem).map((f) => {
+        if (!newLocationWithUpdates[f]) {
+          newLocationWithUpdates[f] = locationItem[
+            f as locationKey
+          ] as locationStateType;
+        }
+        return "";
+      });
+    }
+
+    dispatch(updateLocation(newLocationWithUpdates));
+    history.push("/locations");
+  };
+
+  return (
+    <Root>
+      <Form
+        fields={fields}
+        onSubmit={onSubmitEditLocation}
+        initialValues={locationItem as locationStateType}
+      />
+    </Root>
+  );
+};
+
+export default EditLocation;
